323160abbcea75bd9390a04494e1bd4f75fc8aeb
Pawel Kadluczka <moozzyk@gmail.com>
2016-10-17 18:23:14 -0700

    Adding response caching

--- src/ResponseCachingVerification/Startup.cs
+++ src/ResponseCachingVerification/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -16,12 +12,14 @@ namespace ResponseCachingVerification
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMemoryResponseCacheStore();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole();
+            app.UseResponseCache();
 
             if (env.IsDevelopment())
             {
