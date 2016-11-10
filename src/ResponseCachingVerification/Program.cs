526d51f38001a1337a1fb65e819642075759e799
moozzyk <pawelka@microsoft.com>
2016-11-10 15:11:30 -0800

    Adding Configuration

--- src/ResponseCachingVerification/Program.cs
+++ src/ResponseCachingVerification/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Hosting;
 
 namespace ResponseCachingVerification
@@ -11,7 +12,13 @@ namespace ResponseCachingVerification
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .AddEnvironmentVariables(prefix: "ASPNETCORE_")
+                .Build();
+
             var host = new WebHostBuilder()
+                .UseConfiguration(config)
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
